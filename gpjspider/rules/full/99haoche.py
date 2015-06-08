0a8321a793584b93373a1506fbc0e997ed83d0ab
Webony Xu <jxu@gongpingjia.com>
2015-06-08 23:09:01 +0800

    [REV] updated 99haoche rule

--- gpjspider/rules/full/99haoche.py
+++ gpjspider/rules/full/99haoche.py
@@ -10,129 +10,117 @@ item_rule = {
     "fields": {
         'title': {
             'xpath': (
-                u'//div[@class="car-particular-right clearfix"]/h2/a/text()',
+                text(has_cls('car-particular-right', '/h2/a')),
+                #'//div[@class="car-particular-right clearfix"]/h2/a/text()',
             ),
-            'processors': ['first', 'strip'],
             'required': True,
         },
         'meta': {
             'xpath': (
-                '//meta[@name="description"]/@content',
-                '//meta[@name="Description"]/@content',
+                '//meta[@name="description" or @name="Description"]/@content',
             ),
-            'processors': ['first', 'strip'],
         },
         'year': {
             'xpath': (
                 u'//li/span[contains(text(), "首次上牌")]/../span[@class="righ"]/text()',
             ),
-            'processors': ['first', 'strip', 'year', 'gpjint'],
         },
         'month': {
             'xpath': (
                 u'//li/span[contains(text(), "首次上牌")]/../span[@class="righ"]/text()',
             ),
-            'processors': ['first', 'strip', 'month', 'gpjint'],
         },
         'mile': {
             'xpath': (
                 #u'//li/span[contains(text(), "行驶里程")]/../span[@class="righ"]/text()',
                 u'//div[@class="date"]/ul/li[2]/text()',
             ),
-            'processors': ['first', 'strip', 'mile', 'decimal'],
         },
         'volume': {
             'xpath': (
-                '//div[@class="car-particular-right clearfix"]/h2/a/text()',
+                #'//div[@class="car-particular-right clearfix"]/h2/a/text()',
             ),
-            'processors': ['first', 'strip', 'volume'],
+            'default': '%(title)s',
         },
 
         'color': {
             'xpath': (u'//li/span[contains(text(), "车身颜色")]/../text()',),
-            'processors': ['first', 'strip', '99haoche.color'],
+            #'processors': ['first', '99haoche.color'],
         },
         'control': {
             'xpath': (
                 #u'//li/span[contains(text(), "变速方式")]/../span[@class="righ"]/text()',
                 u'//*[@id="carDetailDiv"]/ul/li[2]/text()',
             ),
-            'processors': ['first', 'strip'],
         },
         'price': {
             'xpath': (
-                #'//p[@class="market-price"]/span[@class="str"]/text()',
+                text(cls('num')),
+                text(cls('price', '/span[1]')),
                 u'//div[@class="price"]/span[1]/text()',
             ),
-            'processors': ['first', 'strip', 'price'],
+            'processors': ['join'],
         },
         'price_bn': {
             'xpath': (
+                text(cls('txt')),
+                text(cls('price', '/span[2]')),
                 '//p[@class="market-price"]/del/text()',
             ),
-            'processors': ['first', 'strip', 'price_bn'],
         },
         'brand_slug': {
             'xpath': (
                 #u'//li/span[contains(text(), "品牌车系")]/../span[@class="righ"]/text()',
                 u'//*[@id="carDetailDiv"]/div[2]/ul[1]/li[4]/text()',
             ),
-            'processors': ['first', 'strip', 'brand_slug'],
         },
         'model_slug': {
             'xpath': (
                 #u'//li/span[contains(text(), "品牌车系")]/../span[@class="righ"]/text()',
                 u'//*[@id="carDetailDiv"]/div[2]/ul[1]/li[4]/text()',
             ),
-            'processors': ['first', 'strip', 'model_slug']
         },
         'city': {
             'xpath': (u'//h2[@name="location"]/a[2]/text()',),
-            'processors': ['first', 'strip', 'city'],
         },
         'region': {
             'xpath': (
                 #u'//span[contains(text(), "看车地址")]/following-sibling::span/text()',
                 u'//p[@class="car-site"]/text()',
             ),
-            #'processors': ['first', 'strip'],
-            'processors': ['last', 'strip'],
+            'processors': ['last'],
         },
         'phone': {
             'xpath': (
+                hidden('carId'),
                 '//a[@name="showPhone2"]/text()',
             ),
-            'processors': ['first', 'strip'],
+            'processors': ['first', '99haoche.phone']
         },
         'company_name': {
             'xpath': (
                 u'//span[contains(text(), "所属商家")]/following-sibling::a/text()',
             ),
-            'processors': ['first', 'strip'],
         },
         'company_url': {
             'xpath': (
                 u'//span[contains(text(), "所属商家")]/following-sibling::a/@href',
             ),
-            'processors': ['first', 'strip'],
         },
         'driving_license': {
             'xpath': (
                 u'//li/span[contains(text(), "行驶证")]/../text()',
             ),
-            'processors': ['first', 'strip'],
         },
         'invoice': {
             'xpath': (
                 u'//li/span[contains(text(), "购车发票")]/../text()',
             ),
-            'processors': ['first', 'strip'],
         },
         'maintenance_record': {
             'xpath': (
                 u'//li/span[contains(text(), "保养记录")]/../text()',
             ),
-            'processors': ['first', 'strip'],
         },
         # 'quality_service': {
         #     'xpath': (
@@ -144,36 +132,36 @@ item_rule = {
             'default': 1,
         },
         'description': {
-            'xpath': ('//div[@class="postscript"]/p[1]/text()',),
-            'processors': ['first', 'strip'],
+            'xpath': (
+                '//div[@class="postscript"]/p[1]/text()',
+            ),
         },
         'imgurls': {
-            'xpath': ('//ul[@id="img_R_L_List"]/li/a/img/@src',),
-            'processors': ['join', 'strip'],
+            'xpath': (
+                '//ul[@id="img_R_L_List"]/li/a/img/@src',
+            ),
+            'processors': ['join'],
         },
         'mandatory_insurance': {
             'xpath': (
                 u'//li/span[contains(text(), "交强险到期时间")]/../text()',
             ),
-            'processors': ['first', 'strip'],
         },
         'business_insurance': {
             'xpath': (
                 u'//li/span[contains(text(), "商业险到期时间")]/../text()',
             ),
-            'processors': ['first', 'strip'],
         },
         'examine_insurance': {
             'xpath': (
                 u'//td[contains(text(), "年检有效期")]/following-sibling::td/text()',
             ),
-            'processors': ['first', 'strip'],
         },
         'transfer_owner': {
             'xpath': (
                 u'//li/span[contains(text(), "是否一手车")]/../text()',
             ),
-            'processors': ['first', 'strip', '99haoche.transfer_owner'],
+            #'processors': ['first', '99haoche.transfer_owner'],
         },
         'source_type': {
             'default': SOURCE_TYPE_SELLER,
@@ -182,7 +170,6 @@ item_rule = {
             'xpath': (
                 u'//li/span[contains(text(), "使用性质")]/../text()',
             ),
-            'processors': ['first', 'strip'],
         },
         # maintenance_desc
     },
@@ -190,14 +177,17 @@ item_rule = {
 
 parse_rule = {
     "url": {
-        "re": (r'http://www\.99haoche\.com/car/\d+\.html',),
-        # 新 url 对应的解析函数
+        "re": (
+            r'http://www\.99haoche\.com/car/\d+\.html',
+        ),
         "step": 'parse_detail',
         'update': True,
         'category': 'usedcar'
     },
     "next_page_url": {
-        "xpath": ('//a[@id="pagenow"]/following-sibling::a[1]/@href', ),
+        "xpath": (
+            '//a[@id="pagenow"]/following-sibling::a[1]/@href',
+        ),
         'format': 'http://www.99haoche.com{0}',
         "step": 'parse',
         # 'incr_pageno': 3,
@@ -206,23 +196,19 @@ parse_rule = {
 
 
 rule = {
-    #==========================================================================
-    #  基本配置
-    #==========================================================================
     'name': u'99好车',
     'domain': '99haoche.com',
-    'start_urls': ['http://www.99haoche.com/quanguo/all/?p=v1'],
+    'start_urls': [
+        #'http://www.99haoche.com/quanguo/all/?p=v1',
+        'http://www.99haoche.com/car/4486289.html',
+    ],
 
-    #==========================================================================
-    #  默认步骤  parse
-    #==========================================================================
     'parse': parse_rule,
-    #==========================================================================
-    #  详情页步骤  parse_detail
-    #==========================================================================
+
     'parse_detail': {
         "item": item_rule,
     }
 }
 
 fmt_rule_urls(rule)
+rule['parse'] = rule['parse_detail']
