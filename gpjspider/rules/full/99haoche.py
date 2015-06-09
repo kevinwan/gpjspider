4f50ed404453a0f1bddafbcdd7635a334d35bbe3
leyle <leyle@leyle.com>
2015-06-09 19:44:04 +0800

    [REV] Updated 99haoche's xpath

--- gpjspider/rules/full/99haoche.py
+++ gpjspider/rules/full/99haoche.py
@@ -22,17 +22,21 @@ item_rule = {
         },
         'year': {
             'xpath': (
-                u'//li/span[contains(text(), "首次上牌")]/../span[@class="righ"]/text()',
+                #u'//li/span[contains(text(), "首次上牌")]/../span[@class="righ"]/text()',
+                text(cls('date', '/ul/li[1]')),
             ),
         },
         'month': {
             'xpath': (
-                u'//li/span[contains(text(), "首次上牌")]/../span[@class="righ"]/text()',
+                #u'//li/span[contains(text(), "首次上牌")]/../span[@class="righ"]/text()',
+                text(cls('date', '/ul/li[1]')),
             ),
+            #'default': '%(year)s',
         },
         'mile': {
             'xpath': (
-                u'//div[@class="date"]/ul/li[2]/text()',
+                #u'//div[@class="date"]/ul/li[2]/text()',
+                text(cls('date', '/ul/li[2]')),
             ),
         },
         'volume': {
@@ -42,44 +46,54 @@ item_rule = {
         },
 
         'color': {
-            'xpath': (u'//li/span[contains(text(), "车身颜色")]/../text()',),
+            'xpath': (
+                #u'//li/span[contains(text(), "车身颜色")]/../text()',
+                has(u'车身颜色', '/..'),
+            ),
         },
         'control': {
             'xpath': (
-                u'//*[@id="carDetailDiv"]/ul/li[2]/text()',
+                #u'//*[@id="carDetailDiv"]/ul/li[2]/text()',
+                text(id_('carDetailDiv', '/ul/li[2]')),
             ),
         },
         'price': {
             'xpath': (
-                text(cls('num')),
+                #text(cls('num')),
                 text(cls('price', '/span[1]')),
-                u'//div[@class="price"]/span[1]/text()',
+                #u'//div[@class="price"]/span[1]/text()',
             ),
             'processors': ['join'],
         },
         'price_bn': {
             'xpath': (
-                text(cls('txt')),
+                #text(cls('txt')),
                 text(cls('price', '/span[2]')),
-                '//p[@class="market-price"]/del/text()',
+                #'//p[@class="market-price"]/del/text()',
             ),
         },
         'brand_slug': {
             'xpath': (
-                u'//*[@id="carDetailDiv"]/div[2]/ul[1]/li[4]/text()',
+                #u'//*[@id="carDetailDiv"]/div[2]/ul[1]/li[4]/text()',
+                text(id_('carDetailDiv', '/div[2]/ul[1]/li[4]')),
             ),
         },
         'model_slug': {
             'xpath': (
-                u'//*[@id="carDetailDiv"]/div[2]/ul[1]/li[4]/text()',
+                text(id_('carDetailDiv', '/div[2]/ul[1]/li[4]')),
+                #u'//*[@id="carDetailDiv"]/div[2]/ul[1]/li[4]/text()',
             ),
         },
         'city': {
-            'xpath': (u'//h2[@name="location"]/a[2]/text()',),
+            'xpath': (
+                #u'//h2[@name="location"]/a[2]/text()',
+                text('h2[@name="location"]/a[2]'),
+            ),
         },
         'region': {
             'xpath': (
-                u'//p[@class="car-site"]/text()',
+                #u'//p[@class="car-site"]/text()',
+                text(cls('car-site')),
             ),
             'processors': ['last'],
         },
@@ -91,7 +105,8 @@ item_rule = {
         },
         'company_name': {
             'xpath': (
-                u'//span[contains(text(), "所属商家")]/following-sibling::a/text()',
+                #u'//span[contains(text(), "所属商家")]/following-sibling::a/text()',
+                after_has(u'所属商家'),
             ),
         },
         'company_url': {
@@ -101,17 +116,20 @@ item_rule = {
         },
         'driving_license': {
             'xpath': (
-                u'//li/span[contains(text(), "行驶证")]/../text()',
+                #u'//li/span[contains(text(), "行驶证")]/../text()',
+                has(u'行驶证', '/..'),
             ),
         },
         'invoice': {
             'xpath': (
-                u'//li/span[contains(text(), "购车发票")]/../text()',
+                #u'//li/span[contains(text(), "购车发票")]/../text()',
+                has(u'购车发票', '/..'),
             ),
         },
         'maintenance_record': {
             'xpath': (
-                u'//li/span[contains(text(), "保养记录")]/../text()',
+                #u'//li/span[contains(text(), "保养记录")]/../text()',
+                has(u'保养记录', '/..'),
             ),
         },
         # 'quality_service': {
@@ -130,28 +148,33 @@ item_rule = {
         },
         'imgurls': {
             'xpath': (
-                '//ul[@id="img_R_L_List"]/li/a/img/@src',
+                #'//ul[@id="img_R_L_List"]/li/a/img/@src',
+                attr(id_('img_R_L_List', '/li/a/img'), 'src'),
             ),
             'processors': ['join'],
         },
         'mandatory_insurance': {
             'xpath': (
-                u'//li/span[contains(text(), "交强险到期时间")]/../text()',
+                #u'//li/span[contains(text(), "交强险到期时间")]/../text()',
+                has(u'交强险', '/..'),
             ),
         },
         'business_insurance': {
             'xpath': (
-                u'//li/span[contains(text(), "商业险到期时间")]/../text()',
+                #u'//li/span[contains(text(), "商业险到期时间")]/../text()',
+                has(u'商业险', '/..'),
             ),
         },
         'examine_insurance': {
             'xpath': (
-                u'//td[contains(text(), "年检有效期")]/following-sibling::td/text()',
+                #u'//td[contains(text(), "年检有效期")]/following-sibling::td/text()',
+                after_has(u'年检有效期'),
             ),
         },
         'transfer_owner': {
             'xpath': (
-                u'//li/span[contains(text(), "是否一手车")]/../text()',
+                #u'//li/span[contains(text(), "是否一手车")]/../text()',
+                has(u'是否一手车', '/..'),
             ),
         },
         'source_type': {
@@ -159,7 +182,8 @@ item_rule = {
         },
         'car_application': {
             'xpath': (
-                u'//li/span[contains(text(), "使用性质")]/../text()',
+                #u'//li/span[contains(text(), "使用性质")]/../text()',
+                has(u'使用性质', '/..'),
             ),
         },
         # maintenance_desc
