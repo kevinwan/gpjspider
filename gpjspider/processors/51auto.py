49f14167d32225f59ad80cc1f124a6ff5d57697b
leyle <leyle@leyle.com>
2015-06-25 16:23:59 +0800

    [ADD] Added 51auto.com's xpath rules

---
+++ gpjspider/processors/51auto.py
@@ -0,0 +1,66 @@
+#!/usr/bin/env python
+#-*-coding:utf-8-*-
+""" 51auto 处理器 """
+import re
+from gpjspider.utils.constants import *
+
+def price_bn(value):
+    """ 新车最低价499.21万[含购置税39.31万] """
+    prices = re.findall('\d+\.?\d*', value)
+    if len(prices) == 2:
+        all_price, tax = prices
+        return str(float(all_price) - float(tax))
+    return ''
+
+def control(value):
+    """ 关键参数：手自一体 5.9L """
+    return value.split(u'：')[-1].split()[0]
+
+def imgurls(value):
+    """ 去掉图片中包含 mid 的图片 """
+    if isinstance(value, (list, tuple)):
+        for img in value[:]:
+            if img.find('-mid') != -1:
+                value.remove(img)
+        return ' '.join(value)
+
+    return value
+
+def color(value):
+    color_base = (
+            u'黑色',
+            u'白色',
+            u'灰色',
+            u'咖啡色',
+            u'红色',
+            u'蓝色',
+            u'绿色',
+            u'黄色',
+            u'橙色',
+            u'香槟色',
+            u'紫色',
+            u'多彩色',
+            u'银色',
+        )
+
+    for c in color_base:
+        if c in value:
+            return c
+    return ''
+
+def source_type(value):
+    type_base = (
+            (u'品牌', SOURCE_TYPE_MANUFACTURER),
+            (u'商家', SOURCE_TYPE_SELLER),
+            (u'个人', SOURCE_TYPE_GONGPINGJIA),
+        )
+
+    for t in type_base:
+        if t[0] in value:
+            return t[1]
+
+    return SOURCE_TYPE_ODEALER
+
+
+
+
---
+++ gpjspider/rules/full/51auto.py
@@ -0,0 +1,265 @@
+# -*- coding: utf-8 -*-
+"""
+51汽车
+"""
+from gpjspider.utils.constants import *
+from .utils import *
+
+_has = lambda x: has(x, '/..')
+
+item_rule = {
+    "class": "UsedCarItem",
+    "fields": {
+        'title': {
+            'xpath': (
+                text(cls('car-title', '/h1')),
+            ),
+            'required': True,
+        },
+        'meta': {
+            'xpath': (
+                '//meta[@name="description" or @name="Description"]/@content',
+            ),
+        },
+        'dmodel': {
+            'xpath': (
+                text(cls('breadcrumb', '/a[5]/span')),
+            ),
+        },
+        'year': {
+            'xpath': (
+                before_has(u'上牌时间'),
+            ),
+            'regex': u'(\d{4})年',
+            'regex_fail': None,
+        },
+        'month': {
+            'xpath': (
+                before_has(u'上牌时间'),
+            ),
+            'regex': u'(\d{1, 2})月',
+        },
+        #'time': {
+            #'xpath': (
+                #text(cls('f10 pr-5')),
+            #),
+        #},
+        'mile': {
+            'xpath': (
+                before_has(u'行驶里程'),
+            ),
+        },
+        'volume': {
+            'xpath': (
+                has(u'关键参数'),
+            ),
+            'regex': r'\d\.?\d?',
+        },
+        'color': {
+            'xpath': (
+                '//title/text()',
+            ),
+            'processors': ['first', '51auto.color'],
+        },
+        'control': {
+            'xpath': (
+                has(u'关键参数'),
+            ),
+            'processors': ['first', '51auto.control'],
+        },
+        'price': {
+            'xpath': (
+                text(cls('price')),
+            ),
+        },
+        'price_bn': {
+            'xpath': (
+                text(cls('new-price')),
+            ),
+            'processors': ['first', '51auto.price_bn'],
+        },
+        'brand_slug': {
+            'xpath': (
+                text(cls('breadcrumb', '/a[3]')),
+            ),
+            'regex': u'二手(.*)',
+        },
+        'model_slug': {
+            'xpath': (
+                text(cls('breadcrumb', '/a[4]')),
+            ),
+            'regex': u'二手(.*)',
+        },
+        'model_url': {
+            'xpath': (
+                href(cls('breadcrumb', '/a[4]')),
+            ),
+            'format': True,
+        },
+        'city': {
+            'xpath': (
+                '//meta[@name="location"]/@content',
+            ),
+            'regex': u'city=(.*);',
+        },
+        'region': {
+            'xpath': (
+                '//*[@class="section-contact"]/text()[2]',
+            ),
+            'regex': u'看车地点：(.*)',
+        },
+        'phone': {
+            'xpath': (
+                text(id_('contact-tel1', '/p')),
+            ),
+        },
+        'contact': {
+            'xpath': (
+                '//*[@class="section-contact"]/text()[1]',
+            ),
+            'regex': u'联系人：(.*)',
+        },
+        'company_name': {
+            'xpath': (
+                text(cls('car-market', '/h1/a')),
+            ),
+        },
+        'company_url': {
+            'xpath': (
+                attr(cls('car-market', '/h1/a'), 'href'),
+            ),
+        },
+        'driving_license': {
+            'xpath': (
+                has(u'行驶证'),
+            ),
+            'regex': u'行驶证：(.*)',
+        },
+        'invoice': {
+            'xpath': (
+                has(u'过户发票'),
+            ),
+            'regex': u'过户发票：(.*)',
+        },
+        #'maintenance_record': {
+            #'xpath': (
+                #_has(u'保养方式'),
+            #),
+        #},
+        'quality_service': {
+            'xpath': (
+               '//*[@class="section-safe"]/span//text()',
+            ),
+            'processors': ['join'],
+        },
+        'description': {
+            'xpath': (
+                text(cls('car-detail-container', '/p[2]')),
+            ),
+        },
+        'imgurls': {
+            'xpath': (
+                attr(cls('lazy'), 'data-original'),
+            ),
+            'processors': ['51auto.imgurls'],
+        },
+        'mandatory_insurance': {
+            'xpath': (
+                has(u'交强险'),
+            ),
+            'regex': u'交强险截止日期：(.*)',
+        },
+        #'business_insurance': {
+            #'xpath': (
+                #_has(u'商业险'),
+            #),
+        #},
+        'examine_insurance': {
+            'xpath': (
+                has(u'车辆年审'),
+            ),
+            'regex': u'车辆年审日期：(.*)',
+        },
+        'transfer_owner': {
+            'xpath': (
+                has(u'过户次数'),
+            ),
+            'regex': u'过户次数：(.*)',
+        },
+        'is_certifield_car': {
+             'default': False,
+             'default': '%(quality_service)s',
+             'default_fail': None,
+        },
+        'source_type': { # 本网站含 品牌认证、商家认证、个人二手车
+            'xpath': (
+                '//title/text()',
+            ),
+            'processors': ['first', '51auto.source_type'],
+        },
+        'car_application': {
+            'xpath': (
+                has(u'车辆用途'),
+            ),
+            'regex': u'车辆用途：(.*)',
+        },
+        #'condition_level': {
+            #'xpath':(
+            #),
+        #},
+        'condition_detail': {
+            'xpath': (
+                has_attr2(u'准新车', 'title'),
+            ),
+        },
+    },
+}
+
+parse_rule = {
+    "url": {
+        "re": (
+            r'http://www\.51auto\.com/buycar/\d+\.html',
+        ),
+        "step": 'parse_detail',
+        'update': True,
+        'category': 'usedcar'
+    },
+    "next_page_url": {
+        "xpath": (
+            has_attr2(u'下一页', 'href'),
+        ),
+        'format': True,
+        "step": 'parse',
+        # 'incr_pageno': 3,
+    },
+}
+
+
+rule = {
+    'name': u'51汽车',
+    'domain': 'www.51auto.com',
+    'base_url': 'http://www.51auto.com',
+    'spider': {
+        'domain': 'www.51auto.com',
+        'download_delay': 2.5,
+    },
+    'start_urls': [
+        'http://www.51auto.com/quanguo/search/?img=y&onsale=y&ordering=publishTime&direction=2', # 在售、有图、按发布时间降序排列
+        #'http://www.51auto.com/buycar/2618559.html',
+        #'http://www.51auto.com/buycar/2622130.html', # 准新车，有商家检测
+        #'http://www.51auto.com/buycar/2645362.html', # 无相关检测
+        #'http://www.51auto.com/buycar/2641587.html', # 商家、过户次数未知、无检测
+        #'http://www.51auto.com/buycar/2646783.html', # 个人车源
+        #'http://www.51auto.com/buycar/2646898.html', # 商家车源
+        #'http://www.51auto.com/buycar/2482736.html', # 品牌车商
+    ],
+
+    'parse': parse_rule,
+
+    'parse_detail': {
+        "item": item_rule,
+    }
+}
+
+fmt_rule_urls(rule)
+#rule['parse'] = rule['parse_detail']
