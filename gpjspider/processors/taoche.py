d3d281d2b84580716682c81871bda7cd6ddf5a03
leyle <leyle@leyle.com>
2015-07-03 17:34:32 +0800

    [REV] Updated taoche's xpath rules

--- gpjspider/processors/taoche.py
+++ gpjspider/processors/taoche.py
@@ -1,7 +1,8 @@
 # -*- coding: utf-8 -*-
 from . import is_certified
-from gpjspider.utils import constants
+from gpjspider.utils.constants import *
 import pdb
+import re
 
 
 def phone(value):
@@ -12,34 +13,58 @@ def phone(value):
     return value
 
 
-def source_type(value):
-    u'''
->>> source_type('http://dealer.che168.com/login.html?backurl=http://www.che168.com/personal/4323708.html#pvareaid=100522#pos=2')
-4
+#def source_type(value):
+    #u'''
+#>>> source_type('http://dealer.che168.com/login.html?backurl=http://www.che168.com/personal/4323708.html#pvareaid=100522#pos=2')
+#4
 
-# >>> source_type(u'http://dealer.che168.com/login.html?backurl=http://www.che168.com/personal/4323708.html#pvareaid=100522#pos=2 质保')
-# 4
-# >>> source_type(u'http://dealer.che168.com/login.html?backurl=http://www.che168.com/personal/4323708.html#pvareaid=100522#pos=2 质保')
-# 4
-# >>> source_type(u'http://dealer.che168.com/login.html?backurl=http://www.che168.com/dealer/106732/4808187.html#pvareaid=100522#pos=2 质保 品牌认证')
-# 4
->>> source_type('/dealer/106732/4808187.html#pvareaid=100520#pos=2')
-5
-    '''
-    # print value
-    st = None 	# constants.SOURCE_TYPE_GONGPINGJIA
-    # pdb.set_trace()
-    try:
-        int(value)
-        st = constants.SOURCE_TYPE_GONGPINGJIA
-    except:
-        if 'Dealer' in value:
-            st = constants.SOURCE_TYPE_ODEALER
-            if is_certified(value):
-                st = constants.SOURCE_TYPE_SELLER
-                if u'品牌认证' in value:
-                    st = constants.SOURCE_TYPE_MANUFACTURER
-    return st
+## >>> source_type(u'http://dealer.che168.com/login.html?backurl=http://www.che168.com/personal/4323708.html#pvareaid=100522#pos=2 质保')
+## 4
+## >>> source_type(u'http://dealer.che168.com/login.html?backurl=http://www.che168.com/personal/4323708.html#pvareaid=100522#pos=2 质保')
+## 4
+## >>> source_type(u'http://dealer.che168.com/login.html?backurl=http://www.che168.com/dealer/106732/4808187.html#pvareaid=100522#pos=2 质保 品牌认证')
+## 4
+#>>> source_type('/dealer/106732/4808187.html#pvareaid=100520#pos=2')
+#5
+    #'''
+    ## print value
+    #st = None 	# constants.SOURCE_TYPE_GONGPINGJIA
+    ## pdb.set_trace()
+    #try:
+        #int(value)
+        #st = constants.SOURCE_TYPE_GONGPINGJIA
+    #except:
+        #if 'Dealer' in value:
+            #st = constants.SOURCE_TYPE_ODEALER
+            #if is_certified(value):
+                #st = constants.SOURCE_TYPE_SELLER
+                #if u'品牌认证' in value:
+                    #st = constants.SOURCE_TYPE_MANUFACTURER
+    #return st
+
+def source_type(value):
+    """ 数据中包含了图片，就是 品牌二手车、仅有链接，就是商家， 否则是个人 """
+    if isinstance(value, (list, tuple)):
+        for val in value:
+            if 'img' in val:
+                return SOURCE_TYPE_MANUFACTURER
+        return SOURCE_TYPE_ODEALER
+    return SOURCE_TYPE_GONGPINGJIA
+
+def description(value):
+    if isinstance(value, (list, tuple)):
+        for val in value[:]:
+            if  u'易车二手车' in val:
+                value.remove(val)
+        return ' '.join([val.strip() for val in value])
+    return value
+
+def mile(value):
+    val = re.search('\d+\.?\d*', value)
+    if val:
+        return val.group() + u'万公里'
+    return 0
+
 
 if __name__ == '__main__':
     import doctest
--- gpjspider/rules/full/taoche.py
+++ gpjspider/rules/full/taoche.py
@@ -1,87 +1,110 @@
 # -*- coding: utf-8 -*-
 from .utils import *
+import time
 
 item_rule = {
     'class': 'UsedCarItem',
     'fields': {
         'title': {
             'xpath': (
-                '//div[@class="tc14-cyxq-tit"]/h3//text()',
-                '/html/head/title/text()',
+                text(cls('tc14-cyxq-tit', '/h3')),
+                #'//div[@class="tc14-cyxq-tit"]/h3//text()',
             ),
             'processors': ['join'],
+            'after': ' - ',
             'required': True,
         },
         'dmodel': {
-            'xpath': (
-                '//div[@class="tc14-cyxq-tit"]/h3/text()',
-                '/html/head/title/text()',
-            ),
-            'after': ' - ',
-            'processors': ['join'],
+            #'xpath': (
+                #'//div[@class="tc14-cyxq-tit"]/h3/text()',
+                #'/html/head/title/text()',
+            #),
+            'default': '%(title)s',
+            #'processors': ['join'],
         },
         'meta': {
             'xpath': (
                 '//meta[@name="description" or @name="Description"]/@content',
             ),
         },
+        'time': {
+            'xpath': (
+                text(cls('tc14-tabtitbox tc14-tab-h53  tc14-cytab clearfix', '/span')),
+            ),
+            'regex': '(\d{2,4}-\d{1,2}-\d{1,2})',
+            'regex_fail': time.strftime('%Y-%m-%d', time.localtime()),
+        },
         'year': {
             'xpath': (
-                u'//*[@id="hidBuyCarDate"]/@value | //*[contains(text(), "上牌")]/following-sibling::text()',
+                after_has(u'上牌时间'),
             ),
         },
         'month': {
             'xpath': (
-                u'//*[@id="hidBuyCarDate"]/@value | //*[contains(text(), "上牌")]/following-sibling::text()',
+                after_has(u'上牌时间'),
             ),
         },
         'mile': {
             'xpath': (
-                u'//*[contains(text(), "行驶里程")]/following-sibling::*/text()',
+                after_has(u'行驶里程'),
             ),
+            'processors': ['first', 'taoche.mile'],
         },
         'volume': {
             'xpath': (
-                u'//*[contains(text(), "发 动 机")]/following-sibling::*/text()',
+                after_has(u'发 动 机'),
+                #u'//*[contains(text(), "发 动 机")]/following-sibling::*/text()',
             ),
         },
         'control': {
             'xpath': (
-                u'//*[contains(text(), "变 速 ")]/following-sibling::*/text()',
+                after_has(u'变 速'),
+                #u'//*[contains(text(), "变 速 ")]/following-sibling::*/text()',
             ),
         },
         'price': {
             'xpath': (
-                '//*[@id="hidPrice"]/@value',
+                attr(id_('hidPrice'), 'value'),
+                #'//*[@id="hidPrice"]/@value',
             ),
         },
         'price_bn': {
             'xpath': (
-                u'//*[@class="jagfcbox"]/p[2]//text()',
+                #u'//*[@class="jagfcbox"]/p[2]//text()',
+                text(cls('jagfcbox', '/p[2]/')),
             ),
             'processors': ['join'],
         },
         'brand_slug': {
             'xpath': (
-                u'//*[contains(text(), "车辆品牌")]/following-sibling::*/text()',
-                '//div[@class="breadnav"]/a[last()-1]/text()',
+                #u'//*[contains(text(), "车辆品牌")]/following-sibling::*/text()',
+                #'//div[@class="breadnav"]/a[last()-1]/text()',
+                after_has(u'车辆品牌'),
             ),
         },
         'model_slug': {
             'xpath': (
-                u'//*[contains(text(), "车辆型号")]/following-sibling::*/text()',
-                '//div[@class="breadnav"]/a[last()]/text()',
+                #u'//*[contains(text(), "车辆型号")]/following-sibling::*/text()',
+                #'//div[@class="breadnav"]/a[last()]/text()',
+                after_has(u'车辆型号'),
+            ),
+        },
+        'model_url': {
+            'xpath': (
+                href(cls('tc14-tal', '/a[5]')),
             ),
         },
         'city': {
             'xpath': (
-                u'//*[contains(text(), "牌照地点")]/following-sibling::*/text()',
-                '//div[@class="breadnav"]/a[2]/text()',
+                #u'//*[contains(text(), "牌照地点")]/following-sibling::*/text()',
+                #'//div[@class="breadnav"]/a[2]/text()',
+                after_has(u'牌照地点'),
             ),
         },
         'phone': {
             'xpath': (
-                '//*[@class="tc14-cydh"]/@style',
+                #'//*[@class="tc14-cydh"]/@style',
+                attr(cls('tc14-cydh'), 'style'),
             ),
             'regex': '(http://cache.taoche.com/buycar/gettel.ashx\?u=\d+&t=\w+)[,&]',
         },
@@ -106,16 +129,17 @@ item_rule = {
         },
         'company_url': {
             'xpath': (
-                '//*[contains(@class,"cycsrzbox")]//h3/a/@href',
-                '//div[@class="cyssbut"]/a/@href',
+                href(cls('cyssbut', '/span/a')),
+                #'//*[contains(@class,"cycsrzbox")]//h3/a/@href',
+                #'//div[@class="cyssbut"]/a/@href',
             ),
         },
-        'maintenance_record': {
-            'xpath': (
-                u'boolean(//*[contains(text(), "定期保养") or contains(text(), "定期4S保养")])',
-            ),
-            'processors': ['first', 'has_maintenance_record'],
-        },
+        #'maintenance_record': {
+            #'xpath': (
+                #u'boolean(//*[contains(text(), "定期保养") or contains(text(), "定期4S保养")])',
+            #),
+            #'processors': ['first', 'has_maintenance_record'],
+        #},
         'quality_service': {
             'xpath': (
                 u'//*[@id="divFuwuContainer"]//*[contains(text(), "质保") or contains(text(), "延保")]/text()',
@@ -124,9 +148,9 @@ item_rule = {
         },
         'description': {
             'xpath': (
-                '//div[@class="cyxqpicdown"]//*[@class="chyxx_text"]/text()',
+                text(cls('chyxx_text')),
             ),
-            'processors': ['join'],
+            'processors': ['taoche.description'],
         },
         'imgurls': {
             'xpath': (
@@ -141,7 +165,8 @@ item_rule = {
         },
         'examine_insurance': {
             'xpath': (
-                u'//*[contains(text(), "年检")]/following-sibling::text()',
+                #u'//*[contains(text(), "年检")]/following-sibling::text()',
+                has(u'年检到期', '/..'),
             ),
         },
         'transfer_owner': {
@@ -152,23 +177,21 @@ item_rule = {
         },
         'car_application': {
             'xpath': (
-                u'//*[contains(text(), "车辆类型")]/following-sibling::text()',
+                #u'//*[contains(text(), "车辆类型")]/following-sibling::text()',
+                after_has(u'车辆类型'),
             ),
-            'processors': ['first', 'after_colon'],
+            #'processors': ['first', 'after_colon'],
         },
         'source_type': {
-            'xpath': (
-                '//*[@id="hidUcarSerialNumber"]/@value | //div[@class="cycsrz" or @class="xbfwicobox"]//text()',
-            ),
-            'processors': ['concat', 'taoche.source_type'],
+            'xpath': [
+                attr(cls('cycs-logo', '/a/img'), 'src'), # 3、品牌认证车商
+                href(cls('cyssbut', '/span/a')),         # 5、普通车商
+            ],
+            'processors': ['taoche.source_type'],
             'default': SOURCE_TYPE_GONGPINGJIA,
         },
         'is_certifield_car': {
-            'xpath': (
-                '//div[@class="cycsrz" or @class="xbfwicobox"]//text()',
-            ),
-            'processors': ['concat'],
-            'default': False,
+            'default': '%(quality_service)s',
         },
     },
 }
@@ -197,6 +220,11 @@ rule = {
         # 'http://www.taoche.com/buycar/b-Dealer15040911803.html',
         # 'http://www.taoche.com/buycar/b-Dealer15041113418.html',
         # 'http://www.taoche.com/buycar/b-Dealer15041214906.html',
+        #'http://www.taoche.com/buycar/p-6373146.html', # 个人
+        #'http://www.taoche.com/buycar/b-DealerJZG1208505T.html', # 商家
+        #'http://www.taoche.com/buycar/b-Dealer15050414791.html', # 商家保障
+        #'http://www.taoche.com/buycar/b-Dealer15051214896.html', # 品牌认证
+        #'http://www.taoche.com/buycar/b-Dealer15070314453.html', # 里程为 百公里内
     ],
     'per_page': 50,
     'pages': 2000,
@@ -238,4 +266,4 @@ rule = {
 }
 
 fmt_rule_urls(rule)
-# rule['parse'] = rule['parse_detail']
+#rule['parse'] = rule['parse_detail']
