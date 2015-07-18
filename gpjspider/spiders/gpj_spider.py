8c1c0de966ca63b1183ae6f7eff657727c330046
David Liu <sliu@gongpingjia.com>
2015-07-18 14:33:55 +0800

    [REV] update gpj spider

--- gpjspider/spiders/gpj_spider.py
+++ gpjspider/spiders/gpj_spider.py
@@ -7,13 +7,33 @@ import os
 
 class GPJSpider(IncrSpider):
     name = 'gpj_spider'
+    site = 'test'
+    site = 'baixing'
+    type_ = 'incr'
 
-    def __init__(self, site, type='incr', *args, **kwargs):
-        rule_name = '%s.%s' % (type, site)
+    def __init__(self, site='', type_=None, *args, **kwargs):
+        site = site or self.site
+        type_ = type_ or self.type_
+        rule_name = '%s.%s' % (type_, site)
         self.name = '%s.%s' % (rule_name, os.getpid())
-        if type == 'full':
+        if type_ == 'full':
             cls = GPJBaseSpider
-        elif type == 'incr':
+        elif type_ == 'incr':
             cls = IncrSpider
             rule_name = site
         cls.__init__(self, rule_name)
+
+
+class IncrGPJSpider(GPJSpider):
+    name = 'gpj_incr_spider'
+
+
+class FullGPJSpider(GPJSpider):
+    name = 'gpj_full_spider'
+    type_ = 'full'
+
+
+class GanjiIncrGPJSpider(IncrGPJSpider):
+    name = 'ganji_incr_spider'
+    site = 'ganji'
+    eval('name = %s_incr_spider' % site)(name)
