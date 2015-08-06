3b3d209cb86e456b6e19de5d3849e338fb6041dc
jayzane <jayzane1993@163.com>
2015-08-06 16:35:23 +0800

    [ADD]new haoche51 the method of xpath

--- gpjspider/rules/full/haoche51.py
+++ gpjspider/rules/full/haoche51.py
@@ -122,6 +122,7 @@ item_rule = {
         'imgurls': {
             'xpath': (
                 '//ul[@class="mrd_ul"]/li/a/img/@data-original',
+                '//div[@class="dtl-wrap scroll-anchor pmatrix"]/*/ul/li/img/@src',
                 # '//div[@class="dt-pictype"]/img/@data-original',
             ),
             # 'processors': ['join', 'raw_imgurls'],
@@ -131,6 +132,7 @@ item_rule = {
             'xpath': (
                 # "//div[@class='cd-pc']/text()[4]",
                 text(cls('ad-tit', '/')),
+                "//div[@class='checker-text']/text()[2]",
             ),
             'processors': ['join'],
             'processors': ['concat'],
@@ -150,18 +152,21 @@ item_rule = {
         'mandatory_insurance': {
             'xpath': (
                 has(u"交强险有效期", prefix="ul/li"),
+                has(u"交强险有效期", prefix="tr/td"),
                 # u'//div[@class="ow-sa1"]/ul/li[contains(text(), "交强")]/text()',
             ),
         },
         'business_insurance': {
             'xpath': (
                 has(u"商业险有效期", prefix="ul/li"),
+                has(u"商业险有效期", prefix="td"),
                 # u'//div[@class="ow-sa1"]/ul/li[contains(text(), "商业")]/text()',
             ),
         },
         'examine_insurance': {
             'xpath': (
                 has(u"年检有效期", prefix="ul/li"),
+                has(u"年检有效期", prefix="td"),
                 # u'//div[@class="ow-sa1"]/ul/li[contains(text(), "年检")]/text()',
             ),
         },
@@ -187,6 +192,7 @@ item_rule = {
         'driving_license': {
             'xpath': (
                 has(u"行驶证", prefix="li"),
+                has(u"行驶证", prefix="td"),
                 #u'//li[contains(text(), "行驶证")]/text()',
             ),
             # 'processors': ['first', 'haoche51.driving_license'],
@@ -195,6 +201,7 @@ item_rule = {
         'invoice': {
             'xpath': (
                 has(u"购车发票", prefix="li"),
+                has(u"购车发票", prefix="td"),
                 #u'//li[contains(text(), "购车发票")]/text()',
             ),
             # 'processors': ['first', 'haoche51.invoice'],
